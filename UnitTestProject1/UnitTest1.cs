86ef0e5ba54fbb1247bf54535de7caaa47948e48
Jakub Stępniak <242536@edu.p.lodz.pl>
2023-03-15 15:13:31 +0100

    początek

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var mainWindow = new TPW_PN_JS.MainWindow();
+
+            // Act
+            var actual = TPW_PN_JS.MainWindow.word;
+
+            // Assert
+            var expected = "Hello World";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
