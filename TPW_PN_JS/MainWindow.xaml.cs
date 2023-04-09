4247b004eab89034f98aea304e4578cb353cb557
PatrykGhot <patryk1259512595@gmail.com>
2023-04-09 23:02:22 +0200

    ex 1

--- TPW_PN_JS/MainWindow.xaml.cs
+++ TPW_PN_JS/MainWindow.xaml.cs
@@ -1,31 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TPW_PN_JS
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public const string word = "Hello World";
+        private List<Kulka> list;
+        private Random random = new Random();
 
         public MainWindow()
         {
+            list = new List<Kulka>();
+            Kulka kulka = new Kulka(200, 300, LosowaPredkosc(), LosowaPredkosc());
+            list.Add(kulka);
+            Kulka kulka2 = new Kulka(130, 100, LosowaPredkosc(), LosowaPredkosc());
+            list.Add(kulka2);
             InitializeComponent();
-            HelloWorldText.Text = word;
+        }
+
+        private double LosowaPredkosc()
+        {
+            return (random.NextDouble() * 2 - 1) * 5;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Inicjalizacja i losowe rozmieszczenie kul
+            // Czyszczenie Canvas
+            BallCanvas.Children.Clear();
+
+            // Rysowanie kul na Canvas
+            foreach (var ball in list)
+            {
+                ball.AktualizujPozycje();
+                var ellipse = new Ellipse
+                {
+                    Width = 20 * 2,
+                    Height = 20 * 2,
+                    Fill = Brushes.Blue
+                };
+
+                Canvas.SetLeft(ellipse, ball.X - 20);
+                Canvas.SetTop(ellipse, ball.Y - 20);
+                BallCanvas.Children.Add(ellipse);
+            }
+        }
+    }
+
+    public class Kulka
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double VelocityX { get; set; }
+        public double VelocityY { get; set; }
+
+        public Kulka(double x, double y, double velocityX, double velocityY)
+        {
+            X = x;
+            Y = y;
+            VelocityX = velocityX;
+            VelocityY = velocityY;
+        }
+
+        public void AktualizujPozycje()
+        {
+            X += VelocityX;
+            Y += VelocityY;
         }
     }
 }
