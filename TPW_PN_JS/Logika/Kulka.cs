0b5320668086c0b7c594611e7a5adb8088640fab
PatrykGhot <patryk1259512595@gmail.com>
2023-04-11 23:29:55 +0200

    Dodanie implementacji Interfejsu ICommand

---
+++ TPW_PN_JS/Logika/Kulka.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+
+namespace TPW_PN_JS.Logika
+{
+    public class Kulka : INotifyPropertyChanged
+    {
+        private double _x;
+        private double _y;
+        private double _predkosc;
+
+        public double X
+        {
+            get => _x;
+            set
+            {
+                _x = value;
+                OnPropertyChanged(nameof(X));
+            }
+        }
+        public double Y
+        {
+            get => _y;
+            set
+            {
+                _y = value;
+                OnPropertyChanged(nameof(Y));
+            }
+        }
+        public double Predkosc
+        {
+            get => _predkosc;
+            set
+            {
+                _predkosc = value;
+                OnPropertyChanged(nameof(Predkosc));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
+
--- TPW_PN_JS/Prezentacja/View/MainWindow.xaml.cs
+++ TPW_PN_JS/Prezentacja/View/MainWindow.xaml.cs
@@ -1,110 +1,106 @@
 ﻿// MainWindow.xaml.cs
-using System;
-using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
-using TPW_PN_JS.Dane;
-using TPW_PN_JS.Logika;
 
 
 namespace TPW_PN_JS.View
 {
     public partial class MainWindow : Window
     {
-        private List<Kulka> list;
-        private Random random = new Random();
-        private DispatcherTimer timer;
-        private IKulkaService kulkaService = new KulkaService();
-
         public MainWindow()
         {
-            list = new List<Kulka>();
             InitializeComponent();
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(2);
-            timer.Tick += Timer_Tick;
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            BallCanvas.Children.Clear();
-
-            foreach (var ball in list)
-            {
-                kulkaService.AktualizujPozycje(ball, Boundary);
-                var ellipse = new Ellipse
-                {
-                    Width = 20 * 2,
-                    Height = 20 * 2,
-                    Fill = Brushes.Blue
-                };
-
-                Canvas.SetLeft(ellipse, ball.X - 20);
-                Canvas.SetTop(ellipse, ball.Y - 20);
-                BallCanvas.Children.Add(ellipse);
-            }
-        }
-
-        private double LosowaPredkosc()
-        {
-            return (random.NextDouble() * 2 - 1) * 5;
-        }
-
-        public void AddBallsButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (int.TryParse(BallCountTextBox.Text, out int numberOfBalls))
-            {
-                list.Clear();
-                BallCanvas.Children.Clear();
-
-                for (int i = 0; i < numberOfBalls; i++)
-                {
-                    double leftBoundary = Boundary.Margin.Left + 20;
-                    double topBoundary = Boundary.Margin.Top + 20;
-                    double rightBoundary = leftBoundary + Boundary.Width - 40;
-                    double bottomBoundary = topBoundary + Boundary.Height - 40;
-
-                    double x = random.Next((int)leftBoundary, (int)rightBoundary);
-                    double y = random.Next((int)topBoundary, (int)bottomBoundary);
-                    Kulka kulka = new Kulka(x, y, LosowaPredkosc(), LosowaPredkosc());
-                    list.Add(kulka);
-
-                    var ellipse = new Ellipse
-                    {
-                        Width = 20 * 2,
-                        Height = 20 * 2,
-                        Fill = Brushes.Blue
-                    };
-
-                    Canvas.SetLeft(ellipse, kulka.X - 20);
-                    Canvas.SetTop(ellipse, kulka.Y - 20);
-                    BallCanvas.Children.Add(ellipse);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Wprowadź poprawną liczbę kulek.");
-
-            }
-            // Inicjalizacja i losowe rozmieszczenie kul
-            int ballCount = Convert.ToInt32(BallCountTextBox.Text);
-            list.Clear();
-            Random rnd = new Random();
-            for (int i = 0; i < ballCount; i++)
-            {
-                double x = rnd.Next((int)Boundary.Margin.Left + 20, (int)(Boundary.Margin.Left + Boundary.Width) - 20);
-                double y = rnd.Next((int)Boundary.Margin.Top + 20, (int)(Boundary.Margin.Top + Boundary.Height) - 20);
-                double velocityX = rnd.NextDouble() * 10 - 5;
-                double velocityY = rnd.NextDouble() * 10 - 5;
-                list.Add(new Kulka(x, y, velocityX, velocityY));
-            }
-            timer.Start();
         }
     }
+    //public partial class MainWindow : Window
+    //{
+    //    private List<Kulka> list;
+    //    private Random random = new Random();
+    //    private DispatcherTimer timer;
+    //    private IKulkaService kulkaService = new KulkaService();
+
+    //    public MainWindow()
+    //    {
+    //        list = new List<Kulka>();
+    //        InitializeComponent();
+    //        timer = new DispatcherTimer();
+    //        timer.Interval = TimeSpan.FromMilliseconds(2);
+    //        timer.Tick += Timer_Tick;
+    //    }
+
+    //    private void Timer_Tick(object sender, EventArgs e)
+    //    {
+    //        BallCanvas.Children.Clear();
+
+    //        foreach (var ball in list)
+    //        {
+    //            kulkaService.AktualizujPozycje(ball, Boundary);
+    //            var ellipse = new Ellipse
+    //            {
+    //                Width = 20 * 2,
+    //                Height = 20 * 2,
+    //                Fill = Brushes.Blue
+    //            };
+
+    //            Canvas.SetLeft(ellipse, ball.X - 20);
+    //            Canvas.SetTop(ellipse, ball.Y - 20);
+    //            BallCanvas.Children.Add(ellipse);
+    //        }
+    //    }
+
+    //    private double LosowaPredkosc()
+    //    {
+    //        return (random.NextDouble() * 2 - 1) * 5;
+    //    }
+
+    //    public void AddBallsButton_Click(object sender, RoutedEventArgs e)
+    //    {
+    //        if (int.TryParse(BallCountTextBox.Text, out int numberOfBalls))
+    //        {
+    //            list.Clear();
+    //            BallCanvas.Children.Clear();
+
+    //            for (int i = 0; i < numberOfBalls; i++)
+    //            {
+    //                double leftBoundary = Boundary.Margin.Left + 20;
+    //                double topBoundary = Boundary.Margin.Top + 20;
+    //                double rightBoundary = leftBoundary + Boundary.Width - 40;
+    //                double bottomBoundary = topBoundary + Boundary.Height - 40;
+
+    //                double x = random.Next((int)leftBoundary, (int)rightBoundary);
+    //                double y = random.Next((int)topBoundary, (int)bottomBoundary);
+    //                Kulka kulka = new Kulka(x, y, LosowaPredkosc(), LosowaPredkosc());
+    //                list.Add(kulka);
+
+    //                var ellipse = new Ellipse
+    //                {
+    //                    Width = 20 * 2,
+    //                    Height = 20 * 2,
+    //                    Fill = Brushes.Blue
+    //                };
+
+    //                Canvas.SetLeft(ellipse, kulka.X - 20);
+    //                Canvas.SetTop(ellipse, kulka.Y - 20);
+    //                BallCanvas.Children.Add(ellipse);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            MessageBox.Show("Wprowadź poprawną liczbę kulek.");
+
+    //        }
+    //        // Inicjalizacja i losowe rozmieszczenie kul
+    //        int ballCount = Convert.ToInt32(BallCountTextBox.Text);
+    //        list.Clear();
+    //        Random rnd = new Random();
+    //        for (int i = 0; i < ballCount; i++)
+    //        {
+    //            double x = rnd.Next((int)Boundary.Margin.Left + 20, (int)(Boundary.Margin.Left + Boundary.Width) - 20);
+    //            double y = rnd.Next((int)Boundary.Margin.Top + 20, (int)(Boundary.Margin.Top + Boundary.Height) - 20);
+    //            double velocityX = rnd.NextDouble() * 10 - 5;
+    //            double velocityY = rnd.NextDouble() * 10 - 5;
+    //            list.Add(new Kulka(x, y, velocityX, velocityY));
+    //        }
+    //        timer.Start();
+    //    }
+    //}
 }
