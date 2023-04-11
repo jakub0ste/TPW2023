0b2693dcd4f3940c6f0189291fa994e15cc7c29a
PatrykGhot <patryk1259512595@gmail.com>
2023-04-11 23:43:21 +0200

    ponowny commit z powodu braku zapisania plikow w VS

--- TPW_PN_JS/Prezentacja/ViewModel/MainViewModel.cs
+++ TPW_PN_JS/Prezentacja/ViewModel/MainViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TPW_PN_JS.Prezentacja.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
